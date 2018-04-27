af7ae67c4a3e1950c482cbc6f5b13920e12d1ee6
Nathan Beckmann <beckmann@cs.cmu.edu>
2018-04-27 02:19:48 -0400

    [company] String representation for companies.

--- company.py
+++ company.py
@@ -4,3 +4,6 @@ class Company:
     def __init__(self, id=0, trains=[]):
         self.id = id
         self.trains = trains
+
+    def __repr__(self):
+        return "(Company: %s, Trains: %s)" % (self.id, self.trains)
